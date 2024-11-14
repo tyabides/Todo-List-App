16bc809f4678ba4ba479c21ae3c92ec8effb5202
tylerjohnson <tyjohnson77@gmail.com>
2024-11-14 12:37:47 -0800

    Add and Complete button Icons

--- gui.py
+++ gui.py
@@ -10,10 +10,12 @@ sg.theme("pythonplus")
 clock = sg.Text('', key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="Yellow",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 12])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(size=2, image_source="complete.png", mouseover_colors="Yellow",
+                            tooltip="Complete Todo", key="Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
