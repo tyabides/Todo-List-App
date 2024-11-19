291ad228415155d0ec9931f5ca5d30c6b6968ac2
tylerjohnson <tyjohnson77@gmail.com>
2024-11-19 10:21:13 -0800

    Implement "Add new todo items" feature

--- web.py
+++ web.py
@@ -3,6 +3,11 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    new_todo = st.session_state["new_todo"] + '\n'
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
@@ -11,4 +16,9 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add a new todo...")
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key="new_todo")
+
+print("hello")
+
+st.session_state
